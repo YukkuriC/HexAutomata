c8a2e297d63c4443f65a430c8695213ebba33ea7
YukkuriC <799433638@qq.com>
2026-03-10 00:35:17 +0800

    lang

--- common/src/main/java/io/yukkuric/hexautomata/items/ItemReactiveFocus.kt
+++ common/src/main/java/io/yukkuric/hexautomata/items/ItemReactiveFocus.kt
@@ -7,6 +7,8 @@ import at.petrak.hexcasting.common.items.storage.ItemFocus
 import io.yukkuric.hexautomata.casting.EntityEventEnv
 import io.yukkuric.hexautomata.events.EventMarker
 import io.yukkuric.hexautomata.events.IHAEvent
+import net.minecraft.locale.Language
+import net.minecraft.network.chat.Component
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Rarity
@@ -23,5 +25,10 @@ class ItemReactiveFocus(val type: EventMarker, props: Properties) : ItemFocus(pr
         val env = EntityEventEnv(event, player)
         val image = CastingImage().copy(stack = event.initStack())
         CastingVM(image, env).queueExecuteAndWrapIotas(list, player.serverLevel())
+    }
+
+    override fun getName(focus: ItemStack): Component {
+        val nameEvent = Language.getInstance().getOrDefault("hexautomata.events.${type.name}", type.name.capitalize())
+        return Component.translatable("item.hexautomata.reactive_focus.template", nameEvent)
     }
 }
