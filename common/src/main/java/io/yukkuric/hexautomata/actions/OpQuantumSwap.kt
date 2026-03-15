f237a507b2a341fdda2fa39d4925b148da44ee30
YukkuriC <799433638@qq.com>
2026-03-15 23:59:35 +0800

    teleport fx

--- common/src/main/java/io/yukkuric/hexautomata/actions/OpQuantumSwap.kt
+++ common/src/main/java/io/yukkuric/hexautomata/actions/OpQuantumSwap.kt
@@ -1,9 +1,12 @@
 package io.yukkuric.hexautomata.actions
 
+import at.petrak.hexcasting.api.casting.ParticleSpray
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.misc.MediaConstants
 import io.yukkuric.hexautomata.casting.EntityEventEnv
 import io.yukkuric.hexautomata.events.IHAEvent
+import net.minecraft.world.entity.Entity
 
 object OpQuantumSwap : EventLimitedAction() {
     override val argc = 0
@@ -18,7 +21,16 @@ object OpQuantumSwap : EventLimitedAction() {
         }
         val oldPosCaster = caster.position()
         caster.teleportToWithTicket(oldPosTarget.x, oldPosTarget.y, oldPosTarget.z)
-        if (target != caster) target.teleportTo(oldPosCaster.x, oldPosCaster.y, oldPosCaster.z)
+        sprayParticleAt(caster, env)
+        if (target != caster) {
+            target.teleportTo(oldPosCaster.x, oldPosCaster.y, oldPosCaster.z)
+            sprayParticleAt(target, env)
+        }
         return listOf()
+    }
+
+    private fun sprayParticleAt(teleportee: Entity, env: CastingEnvironment) {
+        val targetMiddlePos = teleportee.position().add(0.0, teleportee.eyeHeight / 2.0, 0.0)
+        env.produceParticles(ParticleSpray.burst(targetMiddlePos, 2.0), env.pigment)
     }
 }
