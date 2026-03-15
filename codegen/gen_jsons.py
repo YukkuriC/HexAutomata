f97493c9e65cffe7546dfe51edd845a579857b57
YukkuriC <799433638@qq.com>
2026-03-15 17:44:29 +0800

    script to generate repeating jsons

---
+++ codegen/gen_jsons.py
@@ -0,0 +1,82 @@
+import os, json, yaml
+
+
+if 'paths':
+    ROOT_RESOURCES = '../common/src/main/resources'
+
+    # assets
+    ROOT_ASSETS = f'{ROOT_RESOURCES}/assets/hexautomata'
+    DIR_MODELS = f'{ROOT_ASSETS}/models/item/reactive_focus'
+
+    # data
+    ROOT_DATA = f'{ROOT_RESOURCES}/data/hexautomata'
+    DIR_RECIPES = f'{ROOT_DATA}/recipes/brainsweep/reactive_focus'
+
+    # resloc
+    ID_PRE = 'hexautomata:item/reactive_focus'
+
+if 'helpers':
+
+    class ModelBuilder:
+        def __init__(self, parent="item/generated"):
+            self.data = {}
+            self.parent(parent)
+
+        def parent(self, parent):
+            self.data['parent'] = parent
+            return self
+
+        def textures(self, layers):
+            textures = self.data['textures'] = self.data.get('textures', {})
+            for i, tex in enumerate(layers):
+                if not tex:
+                    continue
+                textures[f'layer{i}'] = tex
+            return self
+
+        def overrides(self, overrides):
+            self.data['overrides'] = overrides
+            return self
+
+    def dump_json(path, obj):
+        os.makedirs(os.path.dirname(path), exist_ok=1)
+        with open(path, 'w', encoding='utf-8') as f:
+            json.dump(obj, f, indent=2)
+
+    def dump_brainsweep(
+        id, entity, cost=1000000, src_block='hexcasting:akashic_record'
+    ):
+        obj = {
+            "type": "hexcasting:brainsweep",
+            "blockIn": {"type": "block", "block": src_block},
+            "cost": cost,
+            "entityIn": {"type": "entity_type", "entityType": entity},
+            "result": {"name": f"hexautomata:reactive_focus/{id}"},
+        }
+        dump_json(f'{DIR_RECIPES}/{id}.json', obj)
+
+    def dump_models(id, color):
+        dir_base = f'{DIR_MODELS}/{id}.json'
+        builder = ModelBuilder(f'{ID_PRE}/idle/{id}').overrides(
+            [
+                {
+                    "model": f"{ID_PRE}/active/{id}",
+                    "predicate": {"hexautomata:data": 1.0},
+                }
+            ]
+        )
+        dump_json(f'{DIR_MODELS}/{id}.json', builder.data)
+
+        for mode in 'active', 'idle':
+            builder = ModelBuilder(f'{ID_PRE}/_{mode}').textures(
+                [f'hexcasting:item/colorizer/dye_{color}']
+            )
+            dump_json(f'{DIR_MODELS}/{mode}/{id}.json', builder.data)
+
+
+with open('data.yaml') as f:
+    data = yaml.load(f, yaml.Loader)
+
+for event in data:
+    dump_brainsweep(event['id'], event['entity'])
+    dump_models(event['id'], event['color'])
