9857e572c7fa0789ab477ff285fa9f0f219f2a90
YukkuriC <799433638@qq.com>
2026-03-20 21:26:16 +0800

    fix fabric projectile glitches (fixes #2)

--- fabric/src/main/java/io/yukkuric/hexautomata/fabric/mixin/MixinProjectile.java
+++ fabric/src/main/java/io/yukkuric/hexautomata/fabric/mixin/MixinProjectile.java
@@ -1,6 +1,7 @@
 package io.yukkuric.hexautomata.fabric.mixin;
 
-import io.yukkuric.hexautomata.events.*;
+import io.yukkuric.hexautomata.events.BuiltinEventMarker;
+import io.yukkuric.hexautomata.events.CommonEventsHandler;
 import io.yukkuric.hexautomata.fabric.events.HAEventsFabric;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
@@ -25,7 +26,7 @@ public abstract class MixinProjectile extends Entity {
 
     @Inject(method = "onHit", at = @At("HEAD"))
     void hookOnHit(HitResult hitResult, CallbackInfo ci) {
-        if (!(getOwner() instanceof ServerPlayer player)) return;
+        if (hitResult.getType() == HitResult.Type.MISS || !(getOwner() instanceof ServerPlayer player)) return;
         var event = new HAEventsFabric.ProjectileHit(Projectile.class.cast(this), hitResult);
         if (event.getInvalid()) return;
         CommonEventsHandler.get(BuiltinEventMarker.PROJECTILE_HIT.INSTANCE).invoke(player, event);
