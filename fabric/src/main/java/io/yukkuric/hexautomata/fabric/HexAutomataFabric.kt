a29f8b83a09b5eeb9d33550e21a81cd3f4773bf6
YukkuriC <799433638@qq.com>
2026-03-29 00:41:49 +0800

    bugfix: crashing in dedicated server crafting focus nexus

--- fabric/src/main/java/io/yukkuric/hexautomata/fabric/HexAutomataFabric.kt
+++ fabric/src/main/java/io/yukkuric/hexautomata/fabric/HexAutomataFabric.kt
@@ -1,6 +1,7 @@
 package io.yukkuric.hexautomata.fabric
 
 import at.petrak.hexcasting.common.lib.hex.HexActions
+import at.petrak.hexcasting.common.msgs.IMessage
 import io.yukkuric.hexautomata.HexAutomata
 import io.yukkuric.hexautomata.HexAutomata.IAPI
 import io.yukkuric.hexautomata.HexAutomata.commonInit
@@ -10,12 +11,18 @@ import io.yukkuric.hexautomata.blocks.BrainsweepIntermediate
 import io.yukkuric.hexautomata.fabric.events.HAFabricEventsListener
 import io.yukkuric.hexautomata.fabric.interop.TrinketsInterop
 import io.yukkuric.hexautomata.items.HAItems
+import io.yukkuric.hexautomata.network.HAPackets
+import io.yukkuric.hexautomata.network.packet.S2CShowMultiblock
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.core.Registry
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.network.FriendlyByteBuf
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.server.level.ServerPlayer
 
 class HexAutomataFabric : IAPI(), ModInitializer {
     private fun <T : Any> bindReg(reg: Registry<T>, loader: ((ResourceLocation, T) -> Any?) -> Any?) {
@@ -40,10 +47,45 @@ class HexAutomataFabric : IAPI(), ModInitializer {
             HAConfigFabric.setup()
         }
     }
+
+    object Network : HAPackets.Server {
+        init {
+            HAPackets.SERVER = this
+        }
+
+        private fun <T> make(
+            decoder: (FriendlyByteBuf) -> T, handle: (T, ServerPlayer) -> Unit
+        ) = ServerPlayNetworking.PlayChannelHandler { _, player: ServerPlayer, _, buf: FriendlyByteBuf, _ ->
+            handle(decoder(buf), player)
+        }
+
+        override fun sendPacketToPlayer(player: ServerPlayer, packet: IMessage) {
+            ServerPlayNetworking.send(player, packet.fabricId, packet.toBuf())
+        }
+    }
 }
 
 class HexAutomataFabricClient : ClientModInitializer {
     override fun onInitializeClient() {
         HexAutomataClient.load()
+    }
+
+    object Network : HAPackets.Client {
+        init {
+            HAPackets.CLIENT = this
+            ClientPlayNetworking.registerGlobalReceiver(
+                S2CShowMultiblock.ID, make(
+                    S2CShowMultiblock::deserialize, S2CShowMultiblock::handle
+                )
+            )
+        }
+
+        private fun <T> make(
+            decoder: (FriendlyByteBuf) -> T, handler: (T) -> Unit
+        ) = ClientPlayNetworking.PlayChannelHandler { _, _, buf, _ -> handler(decoder(buf)) }
+
+        override fun sendPacketToServer(packet: IMessage) {
+            ClientPlayNetworking.send(packet.fabricId, packet.toBuf())
+        }
     }
 }
--- forge/src/main/java/io/yukkuric/hexautomata/forge/HexAutomataForge.kt
+++ forge/src/main/java/io/yukkuric/hexautomata/forge/HexAutomataForge.kt
@@ -1,14 +1,24 @@
 package io.yukkuric.hexautomata.forge
 
+import at.petrak.hexcasting.common.msgs.IMessage
 import io.yukkuric.hexautomata.HexAutomata
 import io.yukkuric.hexautomata.HexAutomataClient
 import io.yukkuric.hexautomata.forge.events.HAForgeEventsListener
 import io.yukkuric.hexautomata.forge.interop.CuriosInterop
+import io.yukkuric.hexautomata.network.HAPackets
+import io.yukkuric.hexautomata.network.packet.S2CShowMultiblock
+import net.minecraft.server.level.ServerPlayer
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.ModList
 import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+import net.minecraftforge.network.NetworkEvent
+import net.minecraftforge.network.NetworkRegistry
+import net.minecraftforge.network.PacketDistributor
+import net.minecraftforge.network.simple.SimpleChannel
+import java.util.function.BiConsumer
+import java.util.function.Supplier
 
 @Mod(HexAutomata.MOD_ID)
 class HexAutomataForge : HexAutomata.IAPI() {
@@ -16,9 +26,46 @@ class HexAutomataForge : HexAutomata.IAPI() {
         HAForgeEventsListener.load()
         HAConfigForge.register(ModLoadingContext.get())
         HexAutomata.tryLoadInterop("curios", CuriosInterop::run)
+        Network // hook init
     }
 
     override fun modLoaded(id: String) = ModList.get().isLoaded(id)
+
+    object Network : HAPackets.Client, HAPackets.Server {
+        const val PROTOCOL_VERSION: String = "1"
+        val CHANNEL: SimpleChannel = NetworkRegistry.newSimpleChannel(
+            HexAutomata.modLoc("network"), { PROTOCOL_VERSION }, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals
+        )
+
+        init {
+            HAPackets.CLIENT = this
+            HAPackets.SERVER = this
+
+            // packets
+            var idx = 0
+
+            // clipboard
+            // server show patchouli multiblock
+            CHANNEL.registerMessage(
+                idx++,
+                S2CShowMultiblock::class.java,
+                S2CShowMultiblock::serialize,
+                S2CShowMultiblock::deserialize,
+                makeS2C(S2CShowMultiblock::handle)
+            )
+        }
+
+        private fun <T> makeS2C(consumer: (T) -> Unit) =
+            BiConsumer { packet: T, ctx: Supplier<NetworkEvent.Context> ->
+                consumer(packet)
+                ctx.get().packetHandled = true
+            }
+
+        override fun sendPacketToServer(packet: IMessage) = CHANNEL.sendToServer(packet)
+
+        override fun sendPacketToPlayer(player: ServerPlayer, packet: IMessage) =
+            CHANNEL.send(PacketDistributor.PLAYER.with { player }, packet)
+    }
 }
 
 object HexAutomataForgeClient {
