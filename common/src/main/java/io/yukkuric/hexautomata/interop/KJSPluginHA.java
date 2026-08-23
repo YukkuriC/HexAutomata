8ae700b30602299d31fb7acd551ee1794a3e3a7e
YukkuriC <799433638@qq.com>
2026-08-24 00:22:26 +0800

    fix kubejs dep version

--- common/src/main/java/io/yukkuric/hexautomata/interop/KJSPluginHA.java
+++ common/src/main/java/io/yukkuric/hexautomata/interop/KJSPluginHA.java
@@ -1,15 +1,14 @@
 package io.yukkuric.hexautomata.interop;
 
-import dev.latvian.mods.kubejs.KubeJSPlugin;
-import dev.latvian.mods.kubejs.script.BindingsEvent;
-import dev.latvian.mods.kubejs.script.ScriptType;
+import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingRegistry;
 import io.yukkuric.hexautomata.action_patch.HAPatches;
 import io.yukkuric.hexautomata.action_patch.PatchAction;
 import io.yukkuric.hexautomata.action_patch.brainsweep.BrainsweepCallback;
 
-public class KJSPluginHA extends KubeJSPlugin {
-    public void registerBindings(BindingsEvent event) {
-        if (event.manager.scriptType == ScriptType.CLIENT) return;
+public class KJSPluginHA implements KubeJSPlugin {
+    public void registerBindings(BindingRegistry event) {
+        if (event.type().isClient()) return;
         event.add("BrainsweepCallback", BrainsweepCallback.class);
         event.add("PatchAction", PatchAction.class);
         event.add("HAPatches", HAPatches.INSTANCE);
